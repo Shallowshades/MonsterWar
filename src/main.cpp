764d3fbe836fc07085bb6c7f1431c9123e027bd9
Shallowshades <2923024292@qq.com>
2026-03-16 16:33:05 +0800

    ADD : ImGui 基础用法 (参考ImGui::ShowDemoWindow)

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,157 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 #include <spdlog/spdlog.h>
-#include <entt/entt.hpp>
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
 
-struct position {
-	float x;
-	float y;
-};
+void ImGuiWindow1() {
 
-struct velocity {
-	float dx;
-	float dy;
-};
+	static float volume_value = 0.5f;
+	ImGui::Begin("窗口1");
+	ImGui::Text("这是第一个窗口");
+	ImGui::SetWindowFontScale(1.5f);
+	if (ImGui::Button("按钮1", ImVec2(200, 60))) {
+		spdlog::info("按钮1被点击");
+	}
+	ImGui::SetWindowFontScale(1.0f);
+	if (ImGui::SliderFloat("音量", &volume_value, 0.0f, 1.0f)) {
+		spdlog::info("音量被调整: {}", volume_value);
+	}
+	ImGui::End();
+}
 
-// 新增一个组件来存储实体的“标签”
-// 这里用 entt::hashed_string 作为标签的类型 (不推荐这样用)
-// struct tag {
-//     entt::hashed_string value;   // 它只保留字符串的“指针”，不保证字符串不会被销毁（类似std::string_view）
-// };                               // 将来使用value.data() 很可能为空
+void ImGuiWindow2(SDL_Renderer* renderer) {
+	ImGui::Begin("窗口2");
+	// 显示图片
+	auto texture = IMG_LoadTexture(renderer, "assets/textures/Buildings/Castle.png");
+	if (texture) {
+		ImGui::Image(texture, ImVec2(128, 128));
+	}
+	else {
+		ImGui::Text("无法加载图片");
+	}
+	ImGui::End();
+}
 
-// 确保字符串不被销毁的正确做法：value用于储存，id用于比较/查询
-struct tag {
-	entt::id_type id;
-	std::string value;
-};
+void ImGuiOptionalSettings() {
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-int main() {
-	// 使用 using namespace 来简化哈希字符串字面量的使用
-	using namespace entt::literals;
+	// 设置 ImGui 主题
+	ImGui::StyleColorsDark();
+	// ImGui::StyleColorsLight();
 
-	entt::registry registry;
+	// 设置缩放
+	float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());     // 与系统缩放一致
+	// float main_scale = 1.0f;				// 或者直接设置更加稳定
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(main_scale);        // 固定样式缩放比例。
+	style.FontScaleDpi = main_scale;        // 设置初始字体缩放比例。
 
-	// 创建实体并添加基础组件
-	entt::entity player = registry.create();
-	registry.emplace<position>(player, 10.f, 20.f);
-	registry.emplace<velocity>(player, 1.f, 0.5f);
+	// 设置透明度
+	float window_alpha = 0.5f;
 
-	entt::entity enemy = registry.create();
-	registry.emplace<position>(enemy, 100.f, 50.f);
-	registry.emplace<velocity>(enemy, -0.5f, -1.f);
+	// 修改各个UI元素的透明度
+	style.Colors[ImGuiCol_WindowBg].w = window_alpha;
+	style.Colors[ImGuiCol_PopupBg].w = window_alpha;
 
-	// 1. 使用哈希字符串作为组件数据
-	// "player"_hs 和 "enemy"_hs 在编译时就会被转换成一个整数。
-	// 这意味着在运行时，我们比较的是两个整数，速度极快。
-	// hashed_string可以隐式转换为id_type，既可以赋值...
-	registry.emplace<tag>(player, "player"_hs, "player");
-	registry.emplace<tag>(enemy, "enemy"_hs, "enemy");
+	// 为了正确显示中文，我们需要加载支持中文的字体。
+	ImFont* font = io.Fonts->AddFontFromFileTTF(
+		"assets/fonts/VonwaonBitmap-16px.ttf",  // 字体文件路径
+		16.0f,                                  // 字体大小
+		nullptr,                                // 字体配置参数
+		io.Fonts->GetGlyphRangesChineseSimplifiedCommon() // 字符范围
+	);
+	if (!font)
+	{
+		// 如果字体加载失败，回退到默认字体，但中文将无法显示。
+		io.Fonts->AddFontDefault();
+		spdlog::warn("警告：无法加载中文字体，中文字符将无法正确显示。");
+	}
+}
 
-	spdlog::info("=== 使用哈希字符串标签 ===");
+void ImGuiInit(SDL_Window* window, SDL_Renderer* renderer) {
+	// 初始化 ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
 
-	// 2. 通过视图遍历并识别实体
-	auto view = registry.view<const tag, const position>();
+	// 可选配置
+	ImGuiOptionalSettings();
 
-	view.each([](const auto& entity_tag, const auto& pos) {
-		// 我们可以直接比较哈希值（...也可以比较）
-		if (entity_tag.id == "player"_hs) {
-			spdlog::info("找到玩家，位置: ({}, {})", pos.x, pos.y);
+	// 初始化 ImGui 的 SDL3 和 SDL_Renderer3 后端
+	ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+	ImGui_ImplSDLRenderer3_Init(renderer);
+}
+
+void ImGuiLoop(SDL_Renderer* renderer) {
+	// ImGui: 开始新帧
+	ImGui_ImplSDLRenderer3_NewFrame();
+	ImGui_ImplSDL3_NewFrame();
+	ImGui::NewFrame();
+
+	// 显示一个Demo窗口 （UI声明与逻辑交互）
+	ImGui::ShowDemoWindow();
+	ImGuiWindow1();
+	ImGuiWindow2(renderer);
+
+	// ImGui: 渲染
+	ImGui::Render();    // 生成绘图数据
+	ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);  // 执行渲染
+}
+
+void ImGuiShutdown() {
+	// ImGui: 清理工作
+	ImGui_ImplSDLRenderer3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
+}
+
+int main(int, char**) {
+
+	// SDL初始化
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		spdlog::error("SDL_Init Error: {}", SDL_GetError());
+		return 1;
+	}
+	// 创建窗口
+	SDL_Window* window = SDL_CreateWindow("Hello World!", 1280, 800, 0);
+	// 创建渲染器
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+	// 1. ImGui 初始化
+	ImGuiInit(window, renderer);
+
+	// 渲染循环
+	while (true) {
+		SDL_Event event;
+		if (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				break;
+			}
+			// 2. ImGui: 处理 ImGui 事件
+			ImGui_ImplSDL3_ProcessEvent(&event);
 		}
 
-		if (entity_tag.id == "enemy"_hs) {
-			spdlog::info("找到敌人，位置: ({}, {})", pos.x, pos.y);
-		}
-		});
+		// 清屏
+		SDL_RenderClear(renderer);
 
-	// 3. 哈希值与原始值
-	auto player_tag = registry.get<tag>(player);
-	// spdlog::info("玩家标签的哈希值: {}", player_tag.value.value());
-	// spdlog::info("玩家标签的原始文本: {}", player_tag.value.data());
-	spdlog::info("玩家标签的哈希值: {}", player_tag.id);
-	spdlog::info("玩家标签的原始文本: {}", player_tag.value);
+		// 3. 一轮循环内，ImGui 需要做的操作（逻辑+渲染）
+		ImGuiLoop(renderer);
+
+		// 更新屏幕
+		SDL_RenderPresent(renderer);
+	}
+
+	// 4. ImGui 清理
+	ImGuiShutdown();
+
+	// SDL清理并退出
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }
