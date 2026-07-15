4683b1706ca8f0426e16a45676e0645aba54dab3
Shallowshades <2923024292@qq.com>
2026-07-15 22:55:00 +0800

    修复渲染次序

--- src/engine/system/render_system.cpp
+++ src/engine/system/render_system.cpp
@@ -1,20 +1,24 @@
 #include "render_system.h"
 #include "../render/renderer.h"
 #include "../render/camera.h"
+#include "../component/render_component.h"
 #include "../component/transform_component.h"
 #include "../component/sprite_component.h"
 
 namespace engine::system {
 
 	void RenderSystem::update(entt::registry& registry, render::Renderer& renderer, const render::Camera& camera) {
-		auto view = registry.view<component::TransformComponent, component::SpriteComponent>();
+		// 用 RenderComponent 驱动遍历，保证按 YSortSystem 排序后的 (layer, depth) 顺序渲染
+		auto view = registry.view<component::RenderComponent, component::TransformComponent, component::SpriteComponent>();
+		view.use<component::RenderComponent>();
 		for (auto entity : view) {
+			const auto& render = view.get<component::RenderComponent>(entity);
 			const auto& transform = view.get<component::TransformComponent>(entity);
 			const auto& sprite = view.get<component::SpriteComponent>(entity);
-			auto position = transform.mPosition + sprite.mOffset;   // 位置 = 变换组件的位置 + 精灵的偏移
-			auto size = sprite.mSize * transform.mScale;            // 大小 = 精灵的大小 * 变换组件的缩放
+			auto position = transform.mPosition + sprite.mOffset;
+			auto size = sprite.mSize * transform.mScale;
 			renderer.drawSprite(camera, sprite.mSprite, position, size, transform.mRotation);
 		}
 	}
 
-} // namespace engine::system 
+} // namespace engine::system
